9c56b0a5c3ef33b6bf0ff0870f99ff43cecf5a70
Varshit Dusad <dusadv@gmail.com>
2020-12-20 18:35:53 +0530

    Fixing user controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -8,4 +8,4 @@ class RegistrationsController < Devise::RegistrationsController
 	def account_update_params
 		params.require(:user).permit(:name, :username, :email, :password, :password_confirmation, :current_password)
 	end
-
+end
